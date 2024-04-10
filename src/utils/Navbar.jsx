c08ae32a84956277752904c8e0ba3e4fe2be3ee5
Your Name <plalah002@gmail.com>
2024-04-10 12:46:20 +0200

    update navbar

--- src/utils/Navbar.jsx
+++ src/utils/Navbar.jsx
@@ -1,138 +1,96 @@
-import MenuIcon from "@mui/icons-material/Menu";
-import {
-  AppBar,
-  Box,
-  Button,
-  CssBaseline,
-  Divider,
-  Drawer,
-  IconButton,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  Toolbar,
-  Typography,
-} from "@mui/material";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { RiMenu5Line } from "react-icons/ri";
+import { IoClose } from "react-icons/io5";
 
-const navItems = [
-  { text: "Home", link: "/" },
-  { text: "About", link: "/about" },
-  { text: "Services", link: "/services" },
-  { text: "Blog▽", link: "/blog" },
-  { text: "Contact", link: "/contact" },
-];
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
 
-const Navbar = ({ window }) => {
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
+  const handleNav = () => {
+    setNav(!nav);
   };
 
-  const drawer = (
-    <Box sx={{ textAlign: "center" }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        <Link to={"/"}>
-          <img
-            src={logo}
-            alt="Logo"
-            className="logo"
-            style={{ padding: "1rem" }}
-          />
-        </Link>
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item.text} disablePadding>
-            <ListItemButton sx={{ textAlign: "center" }}>
-              <Link
-                to={item.link}
-                style={{ textDecoration: "none", color: "inherit" }}
-              >
-                <ListItemText primary={item.text} />
-              </Link>
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
+  
+ 
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar position="fixed" sx={{ backgroundColor: "#808080" }}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
+    <div className="bg-black h-24 w-full">
+      <div className="text-white flex justify-between items-center max-w-[1240px] mx-auto px-4">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-[110px] pt-2" />
+        </Link>
+        <ul className="hidden md:flex  text-xl cursor-pointer font-bold">
+          <li
+            className="p-4 hover:bg-orange-700 active:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300"
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link to={"/"}>
-              <img
-                src={logo}
-                alt="Logo"
-                className="logo"
-                style={{ width: "4.5rem", padding: '1rem 0 0 0' }}
-              />
-            </Link>
-          </Typography>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            {navItems.map((item) => (
-              <Button key={item.text} sx={{ color: "black" }}>
-                <Link
-                  to={item.link}
-                  style={{
-                    textDecoration: "none",
-                    color: "inherit",
-                    fontSize: "1rem",
-                  }}
-                >
-                  {item.text}
-                </Link>
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <nav>
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true,
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: 240,
-            },
-          }}
+            <Link to="/">Home</Link>
+          </li>
+          <li
+            className="p-4 hover:bg-orange-600 active:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300"
+          >
+            <Link to="/about">About</Link>
+          </li>
+          <li
+            className="p-4 hover:bg-orange-600 active:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300"
+          >
+            <Link to="/services">Services</Link>
+          </li>
+          <li
+            className="p-4 hover:bg-orange-600 active:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300"
+          >
+            <Link to="/blog">Blog▽</Link>
+          </li>
+          <li
+            className="p-4 hover:bg-orange-600 active:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300"
+          >
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <div onClick={handleNav} className="block md:hidden ">
+          {!nav ? <IoClose size={30} /> : <RiMenu5Line size={30} />}
+        </div>
+
+        <div
+          className={
+            !nav
+              ? "fixed left-0 top-0 w-[60%] h-full bg-gray-600 border-r-gray-200 ease-in-out duration-500 md:hidden  z-10"
+              : "fixed left-[-100%] "
+          }
         >
-          {drawer}
-        </Drawer>
-      </nav>
-      <Box component="main" sx={{ p: { md: 1, xs: 0 } }}>
-        <Toolbar />
-        <Typography />
-      </Box>
-    </Box>
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-[150px] p-4" />
+          </Link>
+          <ul className="pt-8">
+            <li
+              className="p-4 border-b border-gray-500 hover:bg-orange-600 active:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300"
+            >
+              <Link to="/">Home</Link>
+            </li>
+            <li
+              className="p-4 border-b border-gray-500 hover:bg-orange-600 active:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300"
+            >
+              <Link to="/about">About</Link>
+            </li>
+            <li
+              className="p-4 border-b border-gray-500 hover:bg-orange-600 active:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300"
+            >
+              <Link to="/services">Services</Link>
+            </li>
+            <li
+              className="p-4 border-b border-gray-500 hover:bg-orange-600 active:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300"
+            >
+              <Link to="/blog">Blog▽</Link>
+            </li>
+            <li
+              className="p-4 hover:bg-orange-600 active:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300"
+            >
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
 
